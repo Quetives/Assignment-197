a0a5a54ba72b9adf076edda33e6665b2fc6d28aa
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-09 18:26:42 -0700

    Add project files.

---
+++ Assignment 197/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_197
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.Amount = 426.23m;
+
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
